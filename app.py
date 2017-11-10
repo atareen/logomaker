f88d1e162b0f6afacb862f525372398501a6fd87
Ammar Tareen <tareen@qbphysicsammar.cshl.edu>
2017-11-10 14:52:02 -0500

    Backing up flask files after updating python anywhere

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python2.7
-from flask import Flask, render_template, request, session, flash
+from flask import Flask, render_template, request,session, flash
 from werkzeug.utils import secure_filename
 import matplotlib
 matplotlib.use('Agg')
@@ -7,19 +7,25 @@ import matplotlib.pyplot as plt
 from io import BytesIO
 import base64
 import os
-import logomaker
+import sys
+sys.path.append('../')
+import Logomaker_python_anywhere_CSV as logomaker
 import uuid
 import shutil
 import sys
-import subprocess
+from numpydoc.docscrape import NumpyDocString
+from make_logo import make_logo
+
+
 
 # this is the name of the flask app. This conventional in the flask frame-work
 app = Flask(__name__)
 
+# this key decrypts the cookie on the client's browser
 app.secret_key = os.urandom(32)
 
 # allowed input file extensions
-ALLOWED_EXTENSIONS = set(['txt', 'fasta', 'fa'])
+ALLOWED_EXTENSIONS = set(['txt', 'fasta', 'fa','input'])
 ALLOWED_PARAM_EXTENSIONS = set(['txt'])
 
 # handler methods for checking file extensions
@@ -31,6 +37,8 @@ def allowed_param_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_PARAM_EXTENSIONS
 
+# global variable gotten from radio button. Run this by Justin
+# update: tis will be replaced by the functionality via metadata
 
 '''
 This is the default route that is hit when the default URL is entered into the browser.
@@ -42,25 +50,37 @@ POST, the method re-renders the output page with the updated logo.
 @app.route('/', methods=['GET','POST'])
 def index():
 
+    # radio button state and file format
+    radioState = ''
+    fileFormat = ''
+
     # handle creation of temp files
     if 'uid' in session:
-        tempFile = str(session['uid']) + ".fasta"
+        print("Temp file already created")
+        tempFile = str(session['uid']) + ".input"
         tempStyleFile = str(session['uid']) + ".txt"
         logFile = str(session['uid']) + ".log"
+        metaDataFile = str(session['uid']) + ".meta"
         sys.stderr = WarningsLogger(logFile)
-        print("Temp file already made")
     else:
         session['uid'] = uuid.uuid4()
-        tempFile = str(session['uid'])+".fasta"
+        tempFile = str(session['uid'])+".input"
         shutil.copy('crp_sites.fasta',tempFile)
         tempStyleFile = str(session['uid']) + ".txt"
+        metaDataFile = str(session['uid']) + ".meta"
         logFile = str(session['uid']) + ".log"
-        sys.stderr = WarningsLogger(logFile)
         print("New temp file created")
+        sys.stderr = WarningsLogger(logFile)
 
 
-    #print("SECRET KEY: ",app.secret_key)
-    # name of the file that gets passed onto make stylized logo
+    #if my_file.is_file():
+    # read meta data file if not empty
+    #print("Metadata File Size: ",os.path.getsize(metaDataFile))
+
+    # give the data file the temp name
+    dataFileName = tempFile
+
+    # name of the style file that gets passed onto make stylized logo
     # all parameter values are contained in this.
     #style_file = 'parameters_file.txt'
     style_file = tempStyleFile
@@ -68,44 +88,42 @@ def index():
     # default values of parameters
     default_parameters_text = """
 colors : 'Blues'
-logo_type : 'counts'
+logo_type : 'probability'
 axes_type : 'classic'
-font_family : 'sans-serif'
+font_family : 'Dejavu Sans'
 font_weight : 'heavy'
 use_tightlayout : True
 """
 
-    # access global variable, which will update if user uploads
-    # new file
-    #global dataFileName
-
-    # this file name needs to be unique to the session, e.g. time stamp?
-    #dataFileName = 'crp_sites.fasta'
-    dataFileName = tempFile
-    #print(" Beginning: ",dataFileName)
-
-    if 'uid' in session:
-        print("Session Indeks: ",session['uid'])
-    else:
-        session['uid'] = uuid.uuid4()
-        print("Session Indeks after set: ",session['uid'])
-
-
+    # this value will received from radio button choice
+    #fileFormat = 'fasta'
     # process the post from html
     if request.method == 'POST':
 
-        # if parameter button is hit, get updated values
+        # if update parameter button is hit, get updated values
         if str(request.form.get("parameterButton")) == 'Update logo':
+
+            print("Request: ",request.data)
+
             updatedParametes = request.form['paramsTextArea']
             print("Hitting parameter button ")
             with open(style_file, "w") as text_file:
                 text_file.write(updatedParametes)
 
-            print("in parameter update, filename:", dataFileName)
-
         # elif upload data button is hit, upload new data
+        # but put new data in temp file
         elif str(request.form.get("dataUploadButton")) == 'Upload Data':
             print("Hitting Upload button ")
+
+            # read radio button value
+            if request.form.getlist('fileformat'):
+                fileFormat = str(request.form['fileformat'])
+
+                # write state to file
+                with open(metaDataFile,'w') as md:
+                    md.write(fileFormat)
+                #print(fileFormat)
+
 
             # get file name
             f = request.files['file']
@@ -120,12 +138,14 @@ use_tightlayout : True
             elif len(f.filename) is 0:
                 flash(" Please select a file to upload ")
 
+            elif len(fileFormat) is 0:
+                flash("Please choose a radio button")
+
             else:
                 # secure filename cleans the name of the uploaded file
                 f.save(secure_filename(f.filename))
-                #dataFileName = f.filename
-                #print("In upload, datafileName: ",dataFileName)
-                # write the contents of the uploaded file in crp_sites.fasta.
+
+                # the following puts uploaded data in the temp file
                 # write from
                 with open(f.filename) as f1:
                     # write to
@@ -133,7 +153,7 @@ use_tightlayout : True
                         for line in f1:
                             f2.write(line)
 
-                # elif parameter button is pressed
+        # elif parameter upload button is pressed
         elif str(request.form.get("parameterUploadButton")) == 'Upload Parameters':
             print("Hitting parameter upload button ")
 
@@ -164,17 +184,35 @@ use_tightlayout : True
 
     # show index page values on get
     elif request.method == 'GET':
+        print("In get ")
         # on page load the first time, data file name is set to
         # the following file. This logo is displayed when the user
         # first arrives on the page
-        #dataFileName = 'crp_sites.fasta'
-        shutil.copy('crp_sites.fasta',tempFile)
+
+        # the following resets things
+        # reset if get
+        shutil.copy('crp_sites.fasta', tempFile)
+
+        radioState=''
+        if len(metaDataFile)!=0 :
+            open(metaDataFile,'w').close()
+
         dataFileName = tempFile
-        print("I am in get and updated the data file name: ",dataFileName)
+
         # write the default parameter values to a temporary style file
         # which gets passed onto make_stylized_logo
         with open(style_file, 'w') as f:
             f.write(default_parameters_text)
+
+
+    # read state from the metadata file
+    try:
+        with open(metaDataFile) as file:
+            radioState = file.read()
+    except IOError as e:
+        print "Unable to open file"
+        # need to do more here in case can't open file
+
 
     # display parameter values in a textarea: 3 steps
     # Note: these steps could be combined into 1 step but
@@ -192,7 +230,6 @@ use_tightlayout : True
     displayParams = []
     for index in range(paramsLength):
         displayParams.append(rawParams[index].split('    '))
-
 
     # display input data in input text area
     # Also in 3 steps (see comments above)
@@ -215,20 +252,23 @@ use_tightlayout : True
     # aren't drawn on top of each other.
     plt.cla()
 
-    # currently the default mat has the values of the pre-existing file.
-    # this will be changed to uploaded mat when I implement the uploading functionality
-    Mat = logomaker.load_alignment(dataFileName)
+    #if str(fileFormat)=='fasta':
+    if radioState == 'fasta':
+        print("Calling logomaker from fasta", dataFileName)
+        logomaker.make_styled_logo(style_file=style_file,fasta_file=dataFileName)
+    #elif str(fileFormat) == 'meme':
+    elif radioState == 'meme':
+        print("Calling logomaker from meme",dataFileName)
+        logomaker.make_styled_logo(style_file=style_file,meme_file=dataFileName)
+    #elif str(fileFormat) == 'csv':
+    elif radioState == 'csv':
+        logomaker.make_styled_logo(style_file=style_file, csv_file=dataFileName)
+    else:
+        print("Calling logomaker from default", dataFileName, " radiostate: ",radioState)
+        logomaker.make_styled_logo(style_file=style_file, fasta_file=dataFileName)
 
-    # draw the logo
-    logo = logomaker.make_styled_logo(style_file=style_file, matrix=Mat)
-    fig, ax_list = plt.subplots(figsize=[8, 2])
-    logo.draw(ax_list)
-
-    # this saves the logo to disk. Although I'm not if we need this.
-    plt.savefig('/home/kinneylab/Logomaker_python_anywhere/square_plot.png')
 
     #save the logo as a stream of bytes which can be passed into
-    # html and decoded as an image.
     logoFigFile = BytesIO()
     plt.savefig(logoFigFile, format='png')
     logoFigFile.seek(0)
@@ -236,52 +276,58 @@ use_tightlayout : True
     # the following contains the actual data passed to the html template
     logoFigData = base64.b64encode(logoFigFile.getvalue())
 
-    # show warnings
-    #flash(str(subprocess.check_output(['tail', '-1', logFile])).strip())
-
     # Show warnings
     with open(logFile) as log:
         flash(log.read())
-
     cleanWarnings(logFile)
 
+    # populate documentation dictionary
+    # this is currently sub-optimal; should only be run once
+    doc = NumpyDocString(make_logo.__doc__)
+    doc_dict = {}
+    valueString = ''
+    for i in range(len(doc.get('Extended Summary'))):
+
+        if len(doc.get('Extended Summary')[i]) != 0:
+            valueString += doc.get('Extended Summary')[i]
+        else:
+            key = valueString.partition(' ')[0]
+            value = " ".join(valueString.split())
+            # print key ,":",value
+            doc_dict[key] = value
+            valueString = ''
+    # the following to avoid overflows
+    plt.close('All')
     # render the template with logo data
-    return render_template('output.html', result=logoFigData, paramsLength=paramsLength, displayParams=displayParams,displayInput=displayInput, inputDataLength=inputDataLength)
+    return render_template('output.html', result=logoFigData, paramsLength=paramsLength, displayParams=displayParams,
+                           displayInput=displayInput, inputDataLength=inputDataLength,doc_dict=doc_dict,radioState=radioState)
 
 
 @app.before_first_request
 def before_first_request():
+    '''
+    # attempt to create unique session id
+    # and make temp file
+    # if it's not made here, for some weird server issue, make again in index
     session['uid'] = uuid.uuid4()
-
-    tempFile = str(session['uid']) + ".fasta"
+    tempFile = str(session['uid']) + ".input"
     shutil.copy('crp_sites.fasta', tempFile)
+    '''
+    pass
 
-    #session['uid'] = uuid.uuid4()
-    #print("First request TIMESTAMP1111: ",datetime.now())
-    #print("User agent: ",request.user_agent)
-    #print("IP: ",request.environ['REMOTE_ADDR'])
-    #print("Headers: ", request.headers)
-    #print("Session: ",session)
 
 @app.before_request
 def before_request():
     # here we can do something before every
-    # request is made e.g:
-
-    #print("First request TIMESTAMP: ",datetime.now())
-    #print("User agent: ",request.user_agent)
-    #print("IP: ",request.environ['REMOTE_ADDR'])
-    #print("Headers: ", request.headers)
-    #print("Session: ",session)
-    #print("Session: ",session['uid'])
-    # this doesn't always get set :(
-    #print("Cookies: ",request.cookies.get('session'))
+    # request is made
     pass
+
 
 def cleanWarnings(logFile):
     # erase warnings file before request
     if len(logFile)!=0 :
         open(logFile,'w').close()
+
 
 class WarningsLogger(object):
 
@@ -298,7 +344,5 @@ class WarningsLogger(object):
         self.terminal.flush()
         self.log.flush()
 
-'''
 if __name__ == "__main__":
-    app.run()
-'''
+    app.run(debug=True)
