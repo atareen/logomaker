f7cde9d14df1933e3887c2836e523e2122bc730b
Ammar Tareen <atareen64@gmail.com>
2019-04-04 13:48:24 -0400

    trying pre-hook commit with failures

--- scripts/pre-commit.bash
+++ scripts/pre-commit.bash
@@ -1,7 +1,12 @@
 #!/usr/bin/env bash
 
 echo "Running pre-commit hook"
-# ./scripts/run_tests.bash
 
 cd logomaker/tests
 python functional_tests_logomaker.py
+
+# $? stores exit value of the last command
+if [ $? -ne 0 ]; then
+ echo "Tests must pass before commit!"
+ exit 1
+fi
