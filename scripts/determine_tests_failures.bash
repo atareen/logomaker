90ab56f1f86eff4008de20b12d99d4d5b32dbd2e
Ammar Tareen <atareen64@gmail.com>
2019-04-04 14:53:43 -0400

    trying to commit with failure 6

--- scripts/determine_tests_failures.bash
+++ scripts/determine_tests_failures.bash
@@ -6,14 +6,5 @@ cd "${0%/*}/.."
 
 # echo $1 # prints out first argument to bash script
 
-
-if [ "$1" == "0" ]; then
-	echo $1
-    echo "Commit hook passed!"
-    echo 'Code should be committed...'
-else
-    echo "non-zero functional test failures"
-	echo $1
-    echo "do NOT commit code"
-    echo "Failed!" && exit 1
-fi
+pwd
+$1 > scripts/output.txt
