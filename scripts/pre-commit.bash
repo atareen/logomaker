5f3f437a4d8e6a115ef26aad37fbd0ccfeaa068c
Ammar Tareen <atareen64@gmail.com>
2019-04-04 14:36:23 -0400

    testing commit hook 3

--- scripts/pre-commit.bash
+++ scripts/pre-commit.bash
@@ -7,20 +7,26 @@ echo $1
 if [ -z "$1" ]
 then
     # "No argument supplied"
+    
+    echo 'running python functional tests'
+    
     cd logomaker/tests
 	python functional_tests_logomaker.py
 
 else
 
-if [ "$1" == "0" ]; then
-	echo $1
-    echo "Commit hook passed!"
-    echo 'Code should be committed...'
-else
-    echo "non-zero functional test failures"
-	echo $1
-    echo "do NOT commit code"
-    echo "Failed!" && exit 1
+    echo 'checking whether to commit or not'
+
+	if [ "$1" == "0" ]; then
+		echo $1
+	    echo "Commit hook passed!"
+	    echo 'Code should be committed...'
+	else
+    	echo "non-zero functional test failures"
+		echo $1
+	    echo "do NOT commit code"
+    	echo "Failed!" 
+	    exit 1
 fi
 
 
