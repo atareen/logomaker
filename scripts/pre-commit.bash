0bb553b177027ac3ecd7913eb0e210a4a339048d
Ammar Tareen <atareen64@gmail.com>
2019-04-04 15:12:52 -0400

    commiting with failure

--- scripts/pre-commit.bash
+++ scripts/pre-commit.bash
@@ -6,6 +6,8 @@ python functional_tests_logomaker.py
 
 echo 'about to read error code file'
 
+cd ..
+
 # check the output produces by running functional tests
 error_code=$(<scripts/output_code.txt)
 echo $error_code
