5b0ef222684586953dc64f00c534abec6eeb63e1
Ammar Tareen <atareen64@gmail.com>
2019-04-04 14:15:54 -0400

    trying pre-hook commit with failures

--- scripts/pre-commit.bash
+++ scripts/pre-commit.bash
@@ -14,7 +14,7 @@ echo "printing number of functional test failures:"
 
 # magic line to ensure that we're always inside the root of our application,
 # no matter from which directory we'll run script
-cd "${0%/*}/.."
+#cd "${0%/*}/.."
 
 # echo $1 # prints out first argument to bash script
 
