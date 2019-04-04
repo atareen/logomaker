25cb957a5b878e05a7fb5e40ffc2074e0079e1e0
Ammar Tareen <atareen64@gmail.com>
2019-04-04 14:15:18 -0400

    trying pre-hook commit with failures

--- scripts/pre-commit.bash
+++ scripts/pre-commit.bash
@@ -2,10 +2,38 @@
 
 echo "Running pre-commit hook"
 
-cd logomaker/tests
-python functional_tests_logomaker.py
+if [ $# -eq 0 ]
+  then
+  
+    cd logomaker/tests
+	python functional_tests_logomaker.py
+	
+fi
 
-echo $?
+echo "printing number of functional test failures:"
+
+# magic line to ensure that we're always inside the root of our application,
+# no matter from which directory we'll run script
+cd "${0%/*}/.."
+
+# echo $1 # prints out first argument to bash script
+
+if [ $# -ne 0 ]
+then
+
+if [ "$1" == "0" ]; then
+	echo $1
+    echo "no failures"
+    echo 'Code should be committed...'
+else
+    echo "non-zero functional test failures"
+	echo $1
+    echo "do NOT commit code"
+    #echo "Failed!" && exit 1
+    exit 1
+fi
+
+fi
 
 # $? stores exit value of the last command
 if [ $? -ne 0 ]; then
